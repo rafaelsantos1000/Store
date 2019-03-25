b4e2b47be6e3c65fe515c5f8f80c4f4cea1e5e5a
Rafael Santos <rafaasfs@gmail.com>
2019-03-25 16:51:54 +0000

    Add Toolbar to mobile.

--- Store.UIForms/Store.UIForms/App.xaml.cs
+++ Store.UIForms/Store.UIForms/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Store.UIForms
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- Store.UIForms/Store.UIForms/MainPage.xaml.cs
+++ Store.UIForms/Store.UIForms/MainPage.xaml.cs
@@ -13,5 +13,22 @@ namespace Store.UIForms
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            lblFollowers.Text = (int.Parse(lblFollowers.Text) + 1).ToString();
+        }
+
+        private async void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            ToolbarItem tbi = (ToolbarItem)sender;
+
+            bool result = await Application.Current.MainPage.DisplayAlert("Selecionado!",tbi.Text,"OK","CANCEL");
+
+            if(tbi.Text.Equals("Editar") && result)
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new DemoPage());
+            }
+        }
     }
 }
