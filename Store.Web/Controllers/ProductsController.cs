868e6a2deb01b2c7c98ddb29f5d7b721ac161a0d
Rafael Santos <rafaasfs@gmail.com>
2019-04-22 15:46:58 +0100

    Register new users on web.

--- Store.Web/Controllers/ProductsController.cs
+++ Store.Web/Controllers/ProductsController.cs
@@ -88,8 +88,7 @@ namespace Store.Web.Controllers
 
                 var product = this.ToProduct(view, path);
 
-                //TODO: Change for the logged user
-                product.User = await this.userHelper.GetUserByEmailAsync("rafaelsantos@portugalmail.pt");
+                product.User = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 await this.productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -187,9 +186,7 @@ namespace Store.Web.Controllers
                     }
                     var product = this.ToProduct(view, path);
 
-
-                    //TODO: Change for the logged user
-                    product.User = await this.userHelper.GetUserByEmailAsync("rafaelsantos@portugalmail.pt");
+                    product.User = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                     await this.productRepository.UpdateAsync(product);
                 }
                 catch (DbUpdateConcurrencyException)
